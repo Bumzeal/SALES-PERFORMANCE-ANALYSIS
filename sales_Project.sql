c78deb3a5f7b569a57dbaecb88823418b0de0de2
Oluwabunmi Meshach <78567274+Bumzeal@users.noreply.github.com>
2023-05-18 10:15:45 -0600

    Add files via upload

---
+++ sales_Project.sql
@@ -0,0 +1,16 @@
+Create Table salesRecord(Region Text,
+				  Country Text,
+				  Item_Type Text,
+				  Sales_Channel Text,
+				  Order_Priority text,
+				  Order_Date Date,
+				  Order_ID varchar,
+				   Ship_Date Date,
+				   Units_Sold Int,
+				   Unit_Price Float,
+				   Unit_Cost Float,
+				   Total_Revenue Float,
+				   Total_Cost Float,
+				   Total_Profit Float)
+				   
+				   Select * from salesRecord
